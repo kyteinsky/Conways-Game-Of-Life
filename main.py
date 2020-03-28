ae3a6ae3837dad665ff157e61551011ff3bba094
kyteinsky <kyteinsky@gmail.com>
2020-03-28 06:41:47 +0530

    modified:   main.py 	deleted:    other_fns.py

--- main.py
+++ main.py
@@ -1,128 +1,100 @@
+# My LIBRARY
 from tkinter import *
 import numpy as np
-from time import sleep
-from other_fns import *
 
 window = Tk()
 window.title("Pattern Selector")
-#window.configure(background="black")
-#window.geometry('800x610')
+
 
 rows = 25
-columns = 45
+columns = 30
 size = 25
-sel = 1 # for selection mode
+control = True # for control
+list = [] # keeping track of changed boxes
+i = 0       # generation counter
 
 # Using convolution technique
 kernel = [[1,1,1],
           [1,0,1],
           [1,1,1]]
 
-cs = Canvas(window, width=columns*size, height=rows*size, bd=0, highlightthickness=0)
+cs = Canvas(window, width=columns*size, height=rows*size+40, bd=0, highlightthickness=0)
+cs.pack()
 
 
 matrix = np.zeros(shape=(rows+2, columns+2), dtype=np.int32)
-prev_result = np.zeros((rows,columns), dtype=np.int32)
 result = np.zeros((rows,columns), dtype=np.int32)
+rects = np.zeros((rows,columns), dtype=np.int32)
 
-
-
-def paint():
-
-    print('painted')
+def paint_all():
+    for r in range(rows):
+        for c in range(columns):
+            rects[r,c] = cs.create_rectangle(c*size,r*size,c*size+size,r*size+size,
+                                            outline="#cdcdcd",fill="#ddd",
+                                            tags=str(r)+" "+str(c))
+    ##print('painted all')
 
 def clicked(event):
-    global sel, result, matrix
-    if sel == 1:
-        print("in clicked")
+    global control, result, matrix
+    if control:
+        #print("in clicked")
         if cs.find_withtag(CURRENT):
             r, c = int(cs.gettags(CURRENT)[0]), int(cs.gettags(CURRENT)[1])
-            if result[r,c] == 0:
-                result[r,c] = 1
-                cs.itemconfig(CURRENT, fill="#aaa")
-            else:
-                result[r,c] = 0
-                cs.itemconfig(CURRENT, fill="#ddd")
+            result[r,c] = 1 if result[r,c] == 0 else 0
+            list.append((r,c))
 
-        matrix[1:rows+1, 1:columns+1] = result
+        matrix[1:rows+1, 1:columns+1] = result.copy()
+        ##print(result)
+        paint()
 
-
-    #paint(result)
-
-
-
-
-        #cs.itemconfig(CURRENT, fill="#aaa")
-        #cs.update_idletasks()
-
-        # matrix setting 1 or 0
-
+def paint():
+    global list
+    '''for (r,c) in list:                             # for j in range(len(list)):
+        print(len(list))'''
+    l = tuple(list)
+    for (r,c) in l:
+        if result[r,c] == 0:                       # (r,c) = list[j]
+            cs.itemconfig(rects[r,c], fill="#ddd")
+        else:
+            cs.itemconfig(rects[r,c], fill="#777")
+        list.remove((r,c))
+        #print(j)
+    #print("in paint = {}".format(list))
 
 def do_it():
-    global result, matrix
-    print("in do_it")
-        # playing the Game
-
+    global result, matrix, list, i, control
+    control = False # CLICK EVENT DISABLED
+    i += 1
     for r in range(rows):
         for c in range(columns):
             temp = int(np.sum(np.multiply(matrix[0+r:3+r,0+c:3+c], kernel)))
-            result[0+r,0+c] = 1 if alive(temp) and  else 0
-
-    matrix[1:rows+1, 1:columns+1] = result
-    paint(result)
-
-def go():
-    global sel
-    sel = 2
-    print("sel=", sel)
-    while(sel >= 2):
-        cs.after(1000, do_it())
-        cs.pack()
-        window.title(("Pattern Selector - Generation = ",(sel - 1)))
-        sel += 1
-
-
-#cs.bind("<Key>", do_it)
-
-
-'''
-# EXECUTION BEGINS ##########################################################################################################
-'''
-
-
-paint(result)
+            ##print(np.multiply(matrix[0+r:3+r,0+c:3+c], kernel))
+            if matrix[1+r,1+c] == 1: # It's ALIVE!!
+                if temp == 2 or temp == 3:
+                    state=1
+                else:
+                    state=0
+            else:                    # It's DUDE!!
+                if temp == 3:
+                    state = 1
+                else:
+                    state = 0
+            if state != matrix[1+r,1+c]:
+                result[0+r,0+c] = state
+                list.append((r,c))
+    #print("in do_it = {}".format(list))
+    matrix[1:rows+1, 1:columns+1] = result.copy()
+    paint()
+    #cs.pack()
+    window.title("Pattern Selector | Generation = {}".format(i))
+    cs.after(400, do_it)
 
 
 cs.bind("<Button-1>", clicked)
-cs.bind("<Double-1>", close)
+#cs.after(5000, do_it)
 
-cs.after(5000, go)
+Button(window, text ="START", command = do_it).pack()
 
-cs.pack()
+paint_all()
 
 window.mainloop()
-
-
-
-
-'''
-Label(window, bg='green', text="", relief=SUNKEN, width=2)\
-    .grid(row=row, column=column)
-
-Label(window, text="Presenting the Game of Life",
-        fg="white", bg="black", font="none 12 bold")\
-        .pack(padx=5, side=LEFT)
-
-Label(window, text="Generation = 46",
-        fg="white", bg="black", font="none 12 bold")\
-        .pack(padx=5, side=RIGHT)'''
-'''for r in range(rows):
-    for c in range(columns):
-        cs.create_rectangle(c*size,r*size,c*size+size,r*size+size,
-                            outline="#cdcdcd", fill="#ddd" if c != 25 or r != 15 else "#aaa")'''
-
-
-
-
-
-# Change title according to generation
