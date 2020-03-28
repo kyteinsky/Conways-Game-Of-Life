42013210291ea286b8ff941124ff3070a8a63c87
kyteinsky <kyteinsky@gmail.com>
2020-03-28 08:50:47 +0530

    new file:   config.json 	new file:   main_json.py 	renamed:    main.py -> main_mouse_control.py

--- main_json.py
+++ main_json.py
@@ -1,15 +1,15 @@
 # My LIBRARY
 from tkinter import *
 import numpy as np
+import json
 
 window = Tk()
 window.title("Pattern Selector")
 
 
 rows = 25
-columns = 30
+columns = 35
 size = 25
-control = True # for control
 list = [] # keeping track of changed boxes
 i = 0       # generation counter
 
@@ -21,6 +21,8 @@ kernel = [[1,1,1],
 cs = Canvas(window, width=columns*size, height=rows*size+40, bd=0, highlightthickness=0)
 cs.pack()
 
+with open('config.json') as f:
+    list = json.load(f)["1"]
 
 matrix = np.zeros(shape=(rows+2, columns+2), dtype=np.int32)
 result = np.zeros((rows,columns), dtype=np.int32)
@@ -33,37 +35,32 @@ def paint_all():
                                             outline="#cdcdcd",fill="#ddd",
                                             tags=str(r)+" "+str(c))
     ##print('painted all')
+def init_to_one():
+    global list
+    for r,c in tuple(list):
+        result[r,c] = 1
+    paint()
+    list = []
 
-def clicked(event):
-    global control, result, matrix
-    if control:
-        #print("in clicked")
-        if cs.find_withtag(CURRENT):
-            r, c = int(cs.gettags(CURRENT)[0]), int(cs.gettags(CURRENT)[1])
-            result[r,c] = 1 if result[r,c] == 0 else 0
-            list.append((r,c))
-
-        matrix[1:rows+1, 1:columns+1] = result.copy()
-        ##print(result)
-        paint()
 
 def paint():
     global list
+    #print(list)
     '''for (r,c) in list:                             # for j in range(len(list)):
         print(len(list))'''
-    
-    for (r,c) in tuple(list):
+
+    for r,c in tuple(list):
+        # put 1 in place of those indices ==============================================================
         if result[r,c] == 0:                       # (r,c) = list[j]
             cs.itemconfig(rects[r,c], fill="#ddd")
         else:
             cs.itemconfig(rects[r,c], fill="#777")
-        list.remove((r,c))
+        list.remove([r,c])
         #print(j)
     #print("in paint = {}".format(list))
 
 def do_it():
-    global result, matrix, list, i, control
-    control = False # CLICK EVENT DISABLED
+    global result, matrix, list, i
     i += 1
     for r in range(rows):
         for c in range(columns):
@@ -81,7 +78,7 @@ def do_it():
                     state = 0
             if state != matrix[1+r,1+c]:
                 result[0+r,0+c] = state
-                list.append((r,c))
+                list.append([r,c])
     #print("in do_it = {}".format(list))
     matrix[1:rows+1, 1:columns+1] = result.copy()
     paint()
@@ -90,11 +87,11 @@ def do_it():
     cs.after(400, do_it)
 
 
-cs.bind("<Button-1>", clicked)
 #cs.after(5000, do_it)
 
 Button(window, text ="START", command = do_it).pack()
 
 paint_all()
+init_to_one()
 
 window.mainloop()
